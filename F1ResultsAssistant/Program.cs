54463ccd19cb3de0aa90b11d3909d123e2816f21
Fabio Gottlicher <g.fabio.cz@gmail.com>
2017-04-10 17:59:22 -0600

    asd

--- F1ResultsAssistant/Program.cs
+++ F1ResultsAssistant/Program.cs
@@ -5,6 +5,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Server.Features;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 
 namespace F1ResultsAssistant
 {
@@ -12,8 +16,13 @@ namespace F1ResultsAssistant
     {
         public static void Main(string[] args)
         {
+            var configuration = new ConfigurationBuilder()
+            .AddCommandLine(args)
+            .Build();
+
             var host = new WebHostBuilder()
                 .UseKestrel()
+                .UseConfiguration(configuration)
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
